837ccf524762f975d44a6a1568739fe152019bec
Wesley Runnels <wrunnels@mit.edu>
2020-08-22 13:23:30 -0400

    Add preliminary domino images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import face0 from './face0.png';
+import face1 from './face1.png';
+import face2 from './face2.png';
+import face3 from './face3.png';
+import face4 from './face4.png';
+import face5 from './face5.png';
+import face6 from './face6.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Dominos!</p>
+      <img src={face0} className="domino-face" alt="0" />
+      <img src={face1} className="domino-face" alt="1" />
+      <img src={face2} className="domino-face" alt="2" />
+      <img src={face3} className="domino-face" alt="3" />
+      <img src={face4} className="domino-face" alt="4" />
+      <img src={face5} className="domino-face" alt="5" />
+      <img src={face6} className="domino-face" alt="6" />
     </div>
   );
 }
