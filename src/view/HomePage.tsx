16e8db5e513e1a23fd5762f6575d7b76f1e7e8e4
Wesley Runnels <wrunnels@mit.edu>
2021-09-22 21:44:53 -0400

    Fix rooms view overflow issue

--- src/view/HomePage.tsx
+++ src/view/HomePage.tsx
@@ -52,35 +52,41 @@ export const HomePage = observer((props: IProps) => {
             </div>
             <div className="rooms">
                 <div className="rooms-title">Available Rooms</div>
-                <div className="rooms-table">
-                    <div className="room-details-row-header-item">Name</div>
-                    <div className="room-details-row-header-item">Players</div>
-                    <div className="room-details-row-header-item">Action</div>
-                    {localStore.rooms.map(
-                        (room: { id: string; nPlayers: number }, i) => {
-                            return (
-                                <React.Fragment key={i}>
-                                    {/* just use id for now  */}
-                                    <div className="room-table-element-container room-title">
-                                        {room.id}
-                                    </div>{" "}
-                                    <div className="room-table-element-container room-n-players">
-                                        {room.nPlayers}
-                                    </div>
-                                    <div className="room-table-element-container join-room-button-container">
-                                        <button
-                                            className="room-table-element join-room-button"
-                                            onClick={(e) => {
-                                                onEnterRoom(room.id);
-                                            }}
-                                        >
-                                            Join
-                                        </button>
-                                    </div>
-                                </React.Fragment>
-                            );
-                        }
-                    )}
+                <div className="rooms-table-container">
+                    <div className="rooms-table">
+                        <div className="room-details-row-header-item">Name</div>
+                        <div className="room-details-row-header-item">
+                            Players
+                        </div>
+                        <div className="room-details-row-header-item">
+                            Action
+                        </div>
+                        {localStore.rooms.map(
+                            (room: { id: string; nPlayers: number }, i) => {
+                                return (
+                                    <React.Fragment key={i}>
+                                        {/* just use id for now  */}
+                                        <div className="room-table-element-container room-title">
+                                            {room.id}
+                                        </div>{" "}
+                                        <div className="room-table-element-container room-n-players">
+                                            {room.nPlayers}
+                                        </div>
+                                        <div className="room-table-element-container join-room-button-container">
+                                            <button
+                                                className="room-table-element join-room-button"
+                                                onClick={(e) => {
+                                                    onEnterRoom(room.id);
+                                                }}
+                                            >
+                                                Join
+                                            </button>
+                                        </div>
+                                    </React.Fragment>
+                                );
+                            }
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
